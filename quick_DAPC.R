e76bdec0516731c4f61b22592896faaac641fd6a
Julia G. Harenčár <jharenca@ucsc.edu>
2024-06-27 16:52:23 -0700

    Adding random forest code and start to useless DAPC code.

--- quick_DAPC.R
+++ quick_DAPC.R
@@ -1,2 +1,14 @@
 # quick DAPC 
 
+library(adegenet)
+
+# load gind 
+load("CATIgind.RData")
+
+# data(CATIgind) 
+# class(CATIgind)
+# names(CATIgind)
+
+grp <- find.clusters(CATIgind, max.n.clust=7, n.pca = 50,)
+
+table(pop(CATIgind), grp$grp)
